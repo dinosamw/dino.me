f3d570180af5b91c3af6c062b318b77d13df4cae
huoxianyun <45965814+huoxianyun@users.noreply.github.com>
2022-08-15 11:19:04 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,11 +1,11 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
+  title: 'Dinosamw',
+  author: 'Dean',
+  email: 'dinosamw@gmail.com',
+  link: 'https://dinosamw.notion.site/277f46aa46e84e359480c519f15f28f2',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
-  appearance: 'auto', // ['light', 'dark', 'auto'],
+  appearance: 'light', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
